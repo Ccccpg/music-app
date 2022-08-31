8d76151b81a1112928a2552cc7b7a8986d97a94f
cpg <1713405374@qq.com>
2022-08-31 12:25:10 +0800

    封装了vant组件库的按需引入

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,5 @@
-import { Tab, Tabs, Swipe, SwipeItem, Lazyload } from 'vant';
-const list = [Tab, Tabs, Swipe, SwipeItem, Lazyload]
+import { Tab, Tabs, Swipe, SwipeItem, Lazyload, Icon, Button, List, Tag, Cell } from 'vant';
+const list = [Tab, Tabs, Swipe, SwipeItem, Lazyload, Icon, Button, List, Tag, Cell]
 export default function vue_use_vant(vue) {
   list.forEach((item) => {
     return vue.use(item)
