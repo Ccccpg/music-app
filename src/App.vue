7c0db09db4c95ef8c919fa10a3a9031df0829bb6
cpg <1713405374@qq.com>
2022-08-31 11:03:32 +0800

    去除默认样式

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <van-tabs>
-      <van-tab title="首页">
-        <router-view to="/"></router-view>
-      </van-tab>
-      <van-tab title="我的">我的</van-tab>
-      <van-tab title="广场">内容 3</van-tab>
-      <van-tab title="发现">内容 4</van-tab>
+      <van-tab title="首页" to="/home"></van-tab>
+      <van-tab title="我的" to="/my">我的</van-tab>
+      <van-tab title="广场" to="square">广场</van-tab>
+      <van-tab title="发现" to="">内容 4</van-tab>
     </van-tabs>
+    <router-view></router-view>
   </div>
 </template>
-
+<script>
+import '@/lib/index.css'
+export default {
+  
+}
+</script>
 <style lang="less">
 
 </style>
