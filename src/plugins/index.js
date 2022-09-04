5cd6e469809252d1569fc3e139b2cbfa96d26673
cpg <1713405374@qq.com>
2022-09-04 15:52:59 +0800

    更新了vant组件库的按需引入

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,11 @@
-import { Tab, Tabs, Swipe, SwipeItem, Lazyload, Icon, Button, List, Tag, Cell } from 'vant';
-const list = [Tab, Tabs, Swipe, SwipeItem, Lazyload, Icon, Button, List, Tag, Cell]
+import {
+  Tab, Tabs, Swipe, SwipeItem, Lazyload,
+  Icon, Button, List, Tag, Cell, Image as VanImage,
+  Progress, Popup
+} from 'vant';
+const list = [Tab, Tabs, Swipe, SwipeItem, Lazyload,
+  Icon, Button, List, Tag, Cell, VanImage, Progress
+  , Popup]
 export default function vue_use_vant(vue) {
   list.forEach((item) => {
     return vue.use(item)
