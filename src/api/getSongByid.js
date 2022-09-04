bd6367cde57d82c53b49721c42e811e43ab445e3
cpg <1713405374@qq.com>
2022-09-04 15:51:12 +0800

    封装了获取歌曲相关内容的api

---
+++ src/api/getSongByid.js
@@ -0,0 +1,13 @@
+import axios1 from "@/utils/request";
+export function getSong(sid){
+  return axios1({
+    method:"get",
+    url:`/song/url?id=${sid}`
+  })
+}
+export function getSongLyric(sid){
+  return axios1({
+    method:'get',
+    url:`/lyric?id=${sid}`
+  })
+}
